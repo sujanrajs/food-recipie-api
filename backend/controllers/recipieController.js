68840ad0bede5c88816a417bc4ba9a05ff58c0cc
Sujan Raj Shrestha <sujanr.stha@gmail.com>
2022-08-25 21:42:39 +0300

    add express-async-handler

--- backend/controllers/recipieController.js
+++ backend/controllers/recipieController.js
@@ -1,33 +1,35 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc GET Recipie
 // @route GET /api/recipie
 // @access Public
-const getRecipie = (req, res) => {
+const getRecipie = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get Recipie" });
-};
+});
 
 // @desc Create Recipie
 // @route POST /api/recipie
 // @access Public
-const createRecipie = (req, res) => {
+const createRecipie = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add a text");
   }
   res.status(200).json({ message: "Create Recipie" });
-};
+});
 
 // @desc Update Recipie
 // @route PUT /api/recipie
 // @access Public
-const updateRecipie = (req, res) => {
+const updateRecipie = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update Recipie ${req.params.id}` });
-};
+});
 
 // @desc Delete Recipie
 // @route DELETE /api/recipie
 // @access Public
-const deleteRecipie = (req, res) => {
+const deleteRecipie = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete Recipie ${req.params.id}` });
-};
+});
 
 module.exports = { getRecipie, createRecipie, updateRecipie, deleteRecipie };
